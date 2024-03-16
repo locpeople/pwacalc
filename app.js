49074a1bef777cd182a9bb69f4d5c809f84d9389
jjongman <johan_jongman@hotmail.com>
2024-03-16 10:54:15 +0000

    adding localStorage

--- app.js
+++ app.js
@@ -4,19 +4,31 @@ const parser = math.parser();
 document.addEventListener("DOMContentLoaded", () => {
     const inputbox = document.querySelector(".input");
     const outputbox = document.querySelector(".output");
+    const lines = window.localStorage.getItem("lines");
+    if (lines) {
+        const parsedLines = JSON.parse(lines);
+        inputbox.value = parsedLines.join("\n");
+        outputbox.value = getResults(parsedLines);
+    }
+
     inputbox.addEventListener("keyup", () => {
-        parser.clear();
         const lines = inputbox.value.split("\n");
-        let results = "";
-        lines.forEach(line => {
-            try {
-                const result = parser.evaluate(line);
-                results = results + result.toString() + "\n";
-            }
-            catch(e) { 
-                results += "\n"
-            } 
-        });
-        outputbox.value = results;
+        outputbox.value = getResults(lines);
+        window.localStorage.setItem("lines", JSON.stringify(lines));
     })
 })
+
+function getResults(lines) {
+    parser.clear();
+    let results = "";
+    lines.forEach(line => {
+        try {
+            const result = parser.evaluate(line);
+            results = results + result.toString() + "\n";
+        }
+        catch (e) {
+            results += "\n"
+        }
+    });
+    return results;
+}
