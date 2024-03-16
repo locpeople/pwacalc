c9883e94a2ad9fe3032019eaef7a35eb454a6877
jjongman <johan_jongman@hotmail.com>
2024-03-16 10:36:23 +0000

    using math.js parser

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 const PRECISION = 2;
+const parser = math.parser();
 
 document.addEventListener("DOMContentLoaded", () => {
     const inputbox = document.querySelector(".input");
     const outputbox = document.querySelector(".output");
     inputbox.addEventListener("keyup", () => {
+        parser.clear();
         const lines = inputbox.value.split("\n");
-        try {
-            const results = math.evaluate(lines)
-                .map(x => x.toString())
-                .join("\n");
-
-            outputbox.value = results;
-
-               
-        }
-        catch (e) { }
+        let results = "";
+        lines.forEach(line => {
+            try {
+                const result = parser.evaluate(line);
+                results = results + result.toString() + "\n";
+            }
+            catch(e) { 
+                results += "\n"
+            } 
+        });
+        outputbox.value = results;
     })
 })
